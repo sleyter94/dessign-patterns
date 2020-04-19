3fa587b07af492d760097c396ed477a487e56dd1
Sleyter Sandoval <sleyter94@gmail.com>
2020-04-18 21:52:47 -0500

    Desafio del dia ruta minima

---
+++ src/main/java/edu/excercise/leetcode/minPathSum.java
@@ -0,0 +1,26 @@
+package edu.excercise.leetcode;
+
+public class minPathSum {
+
+  public static void main(String[] args) {
+    int grid[][] = {{1,3,1},{1,5,1},{4,2,1}};
+    System.out.println(minPathSum(grid));
+  }
+
+  public static int minPathSum(int[][] grid) {
+    for(int i=0;i<grid.length;i++){
+      for(int j=0;j<grid[0].length;j++){
+        if(i==0 && j!= 0){
+          grid[i][j] = grid[i][j] + grid[i][j-1];
+        }
+        if(i!=0 && j==0){
+          grid[i][j] = grid[i][j] + grid[i-1][j];
+        }
+        if(i!=0 && j!=0){
+          grid[i][j] = grid[i][j] + Math.min(grid[i-1][j],grid[i][j-1]);
+        }
+      }
+    }
+    return grid[grid.length-1][grid[0].length-1];
+  }
+}
